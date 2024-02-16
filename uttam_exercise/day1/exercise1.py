435f86993c6a3b28ac086b4ed2864f3db5daa1ac
UttamVelani <40419289+uttampatel007@users.noreply.github.com>
2024-02-16 07:46:22 +0530

    updating the read me

--- uttam_exercise/day1/exercise1.py
+++ uttam_exercise/day1/exercise1.py
@@ -1,1 +1,12 @@
 """Hello World"""
+
+# set up git -> windows
+# fork the repo -> https://github.com/nikunjangc/PythonClass
+# clone the repo -> git clone <url>
+# add your code
+# commit your code
+# create pull request
+# 
+
+
+# 
